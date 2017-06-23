ababc0c796566f01cd703574aa92209c4affed12
Michael Mork <morkrom@protonmail.ch>
2017-06-22 17:37:23 -0700

    initial

---
+++ Swift-WebP/Swift-WebP/Swift-WebP.h
@@ -0,0 +1,19 @@
+//
+//  Swift-WebP.h
+//  Swift-WebP
+//
+//  Created by Michael Mork on 6/22/17.
+//  Copyright © 2017 Michael Mork. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Swift-WebP.
+FOUNDATION_EXPORT double Swift_WebPVersionNumber;
+
+//! Project version string for Swift-WebP.
+FOUNDATION_EXPORT const unsigned char Swift_WebPVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Swift_WebP/PublicHeader.h>
+
+
