55c58fab5d8f6d0aa41174e0e847b162726a8c2f
Michael Mork <mmork@DSC-ML-MMORK2.local>
2017-09-14 14:55:48 -0700

    example project

---
+++ SwiftWebP/build/SwiftWebP.build/Release-iphoneos/SwiftWebP.build/DerivedSources/SwiftWebP_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char SwiftWebPVersionString[];
+ extern const double SwiftWebPVersionNumber;
+
+ const unsigned char SwiftWebPVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:SwiftWebP  PROJECT:SwiftWebP-1" "\n";
+ const double SwiftWebPVersionNumber __attribute__ ((used)) = (double)1.;
